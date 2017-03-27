92ca918c49b1805352719cf0ab3d21ef9329d72f
Noemi <andreoli.noemi@gmail.com>
2017-03-27 14:18:13 +0100

    added menu and main-content animations

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,11 +3,27 @@ $(document).ready(function(){
   //burger icon animation
   $("#burger-container").on('click', function(){
     $(this).toggleClass("open");
+  //menu-layer animation
+    animateStuff($(this));
+  });
 
-    if($(this).hasClass('open')){
+  function animateStuff(burger){
+    if(burger.hasClass('open')){
       $('.menu-layer').addClass('menu-down');
+      $('.main-content').removeClass('fade-in');
     }else{
       $('.menu-layer').removeClass('menu-down');
+      $('.main-content').addClass('fade-in');
     }
+  }
+
+  $( ".input-group" ).submit(function(event) {
+    event.preventDefault();
+    if($("#burger-container").hasClass('open')){
+      $("#burger-container").removeClass("open");
+      $('.menu-layer').removeClass('menu-down');
+    }
+
+    $('.main-content').addClass('fade-in');
   });
 });
